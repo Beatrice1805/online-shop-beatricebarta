cf6a94a2504f4b4f1a8469742e278c08ea143230
Beatrice <oprea.beatrice@yahoo.com>
2020-03-03 08:48:46 +0200

    Initial commit

---
+++ src/test/java/org/fastrackit/online/shop/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fastrackit.online.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
