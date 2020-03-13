00fa348c6adf84108113fd7722946939893da41c
Beatrice <oprea.beatrice@yahoo.com>
2020-03-13 17:06:09 +0200

    added updated and delete product methods

--- src/main/java/org/fastrackit/online/shop/service/ProductService.java
+++ src/main/java/org/fastrackit/online/shop/service/ProductService.java
@@ -6,6 +6,7 @@ import org.fastrackit.online.shop.persistance.persistance.ProductRepository;
 import org.fastrackit.online.shop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,17 +40,28 @@ public Product  getProduct(long id){
         LOGGER.info("Retrieving product ()",id );
 
         Optional<Product> productOptional = productRepository.findById(id);
-////
+
 ////        if (productOptional.isPresent()) {
 ////            return productOptional.get();
 ////        } else {
 ////    throw new ResourceNotFoundException("Product" +id + "not found.");
 //}
-return productRepository.findById(id)
+    return productRepository.findById(id)
         //lambda expressions
         .orElseThrow(() -> new ResourceNotFoundException(
                     "Product" +id + "not found."));
-        };
+        }
+        public Product updateProduct(long id, SaveProductRequest request){
+        LOGGER.info("Updating product {}:{}",id , request);
+            Product product = getProduct(id);
+            BeanUtils.copyProperties(request, product);
+            return productRepository.save(product);
+
+        }
+        public void deleteProduct(long id){
+             LOGGER.info("Deleting product {}", id);
+            productRepository.deleteById(id);
+        }
 }
 
 
--- src/test/java/org/fastrackit/online/shop/ProductServiceIntegrationTests.java
+++ src/test/java/org/fastrackit/online/shop/ProductServiceIntegrationTests.java
@@ -1,8 +1,10 @@
 package org.fastrackit.online.shop;
 
 import org.fastrackit.online.shop.domain.Product;
+import org.fastrackit.online.shop.exception.ResourceNotFoundException;
 import org.fastrackit.online.shop.service.ProductService;
 import org.fastrackit.online.shop.transfer.SaveProductRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,19 +23,10 @@ public class ProductServiceIntegrationTests {
     @Test
 
    void createProduct_whenValidRequest_thenProductIsCreated(){
-        SaveProductRequest request = new SaveProductRequest();
-        request.setName("Phone");
-        request.setQuantity(100);
-        request.setPrice(300.5);
+        createProduct();
+    }
 
-        Product product =productService.createProduct(request);
-        assertThat(product,notNullValue());
-        assertThat(product.getId(),greaterThan(0L));
-        assertThat(product.getName(), is(request.getName()));
-        assertThat(product.getPrice(),is(request.getPrice()));
-        assertThat(product.getQuantity(), is(request.getQuantity()));
-}
-@Test
+    @Test
 void createProduct_whenMissingName_thenExceptionIsThrown() {
         SaveProductRequest request = new SaveProductRequest();
         request.setQuantity(1);
@@ -46,8 +39,73 @@ void createProduct_whenMissingName_thenExceptionIsThrown() {
         assertThat("Unexpected exception type. ",
                 e instanceof TransactionSystemException);
     }
+    }
+    @Test
+    void getProduct_whenExistingProduct_thenReturnProduct() {
+        Product product = createProduct();
 
-    System.out.println("test");
+        Product response = productService.getProduct(product.getId());
 
+        assertThat(response, notNullValue());
+        assertThat(response.getId(), is(product.getId()));
+        assertThat(response.getName(), is(product.getName()));
+        assertThat(response.getPrice(), is(product.getPrice()));
+        assertThat(response.getQuantity(), is(product.getQuantity()));
+        assertThat(response.getDescription(), is(product.getDescription()));
+        assertThat(response.getImageURL(), is(product.getImageURL()));
+
+
+    }
+    @Test
+    void getProduct_whenNonExistingProduct_thenThrowResourceNotFoundException(){
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () ->productService.getProduct(99999999));
+
+    }
+@Test
+    void updateProduct_whenValidRequest_thenReturnUpdatedProduct(){
+        Product product = createProduct();
+        SaveProductRequest request = new SaveProductRequest();
+        request.setName(product.getName() + "updated");
+        request.setDescription(product.getDescription() + "updated");
+        request.setPrice(product.getPrice() + 10);
+        request.setQuantity(product.getQuantity() + 10);
+
+
+        Product updatedProduct = productService.updateProduct(product.getId(),request);
+        assertThat( updatedProduct,notNullValue());
+        assertThat(updatedProduct.getId(),is (product.getId()));
+        assertThat(updatedProduct.getName(),is(request.getName()));
+        assertThat(updatedProduct.getDescription(),is(request.getDescription()));
+        assertThat(updatedProduct.getPrice(),is(request.getPrice()));
+        assertThat(updatedProduct.getQuantity(); is(request.getQuantity());
+
+}
+    @Test
+    void deleteProduct_whenExistingProduct_thenProductDoesNotExistAnymore(){
+        Product product = createProduct ();
+
+        productService.deleteProduct(product.getId());
+
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> productService.getProduct(product.getId()));
+
+    }
+
+    private Product createProduct() {
+        SaveProductRequest request = new SaveProductRequest();
+        request.setName("Phone");
+        request.setQuantity(100);
+        request.setPrice(300.5);
+
+        Product product =productService.createProduct(request);
+
+        assertThat(product,notNullValue());
+        assertThat(product.getId(),greaterThan(0L));
+        assertThat(product.getName(), is(request.getName()));
+        assertThat(product.getPrice(),is(request.getPrice()));
+        assertThat(product.getQuantity(), is(request.getQuantity()));
+
+        return product;
     }
 }
